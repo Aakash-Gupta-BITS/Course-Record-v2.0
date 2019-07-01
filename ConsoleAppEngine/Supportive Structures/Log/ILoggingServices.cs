95c83ca1cd29fc75b8a53b8e9879a3b0cc0aaa66
Aakash Gupta <f20180887@pilani.bits-pilani.ac.in>
2019-07-01 15:50:48 +0530

    Log File Class Implemented and sample in App.xaml.cs

---
+++ ConsoleAppEngine/Supportive Structures/Log/ILoggingServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroLog;
+
+namespace ConsoleAppEngine.Log
+{
+    interface ILoggingServices
+    {
+        void WriteLine<T>(string message, LogLevel logLevel = LogLevel.Trace, Exception exception = null);
+    }
+}
---
+++ ConsoleAppEngine/Supportive Structures/Log/LoggingServices.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroLog;
+using MetroLog.Targets;
+
+namespace ConsoleAppEngine.Log
+{
+    public class LoggingServices : ILoggingServices
+    {
+        #region Properties
+        public static LoggingServices Instance { get; }
+
+        public static int RetainDays { get; } = 10;
+
+        public static bool Enabled{ get; set; } = true;
+        #endregion
+
+        static LoggingServices()
+        {
+            Instance = Instance ?? new LoggingServices();
+            LogManagerFactory.DefaultConfiguration.AddTarget(LogLevel.Trace, LogLevel.Fatal, new StreamingFileTarget { RetainDays = RetainDays });
+        }
+
+        public void WriteLine<T>(string message, LogLevel logLevel = LogLevel.Trace, Exception exception = null)
+        {
+            if (Enabled)
+            {
+                var logger = LogManagerFactory.DefaultLogManager.GetLogger<T>();
+                if (logLevel == LogLevel.Trace && logger.IsTraceEnabled)
+                    logger.Trace(message);
+                else if (logLevel == LogLevel.Debug && logger.IsDebugEnabled)
+                    logger.Debug(message);
+                else if (logLevel == LogLevel.Error && logger.IsErrorEnabled)
+                    logger.Error(message);
+                else if (logLevel == LogLevel.Fatal && logger.IsFatalEnabled)
+                    logger.Fatal(message);
+                else if (logLevel == LogLevel.Info && logger.IsInfoEnabled)
+                    logger.Info(message);
+                else if (logLevel == LogLevel.Warn && logger.IsWarnEnabled)
+                    logger.Warn(message);
+            }
+        }
+    }
+}
