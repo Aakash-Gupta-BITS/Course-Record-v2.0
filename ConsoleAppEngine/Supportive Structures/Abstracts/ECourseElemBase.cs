8f9370540c1ae65b4609199a6da62565cab8eda9
Aakash Gupta <f20180887@pilani.bits-pilani.ac.in>
2019-07-01 11:19:33 +0530

    Code Commented

--- ConsoleAppEngine/Supportive Structures/Abstracts/ECourseElemBase.cs
+++ ConsoleAppEngine/Supportive Structures/Abstracts/ECourseElemBase.cs
@@ -12,39 +12,111 @@ namespace ConsoleAppEngine.Abstracts
 {
     public abstract class EElementBase<T> where T : EElementItemBase
     {
+        /// <summary>
+        /// Main list in which entries will be added
+        /// </summary>
         public readonly LinkedList<T> lists = new LinkedList<T>();
+        /// <summary>
+        /// Entry, which is selected in ViewGrid, will be modified or deleted
+        /// </summary>
         protected T ItemToChange { get; set; }
-        public bool IsDestructed => ViewGrid == null;
 
+        /// <summary>
+        /// Grid displyed when user wants to see whole list
+        /// </summary>
         protected Grid ViewGrid;
+        /// <summary>
+        /// Grid displayed when user wants to add or modify some entry
+        /// </summary>
         protected Grid AddGrid;
+        /// <summary>
+        /// ListView in which entries will be displayed in ViewGrid
+        /// </summary>
         protected ListView ViewList;
+        /// <summary>
+        /// Add or Modify button in AddGrid
+        /// </summary>
         protected Button AddButton;
+        /// <summary>
+        /// ContentDialog presented when an entry in selected in ViewList in ViewGrid
+        /// </summary>
         protected readonly ContentDialog contentDialog = new ContentDialog()
         {
             PrimaryButtonText = "Modify",
             SecondaryButtonText = "Delete",
             CloseButtonText = "Ok"
         };
+        /// <summary>
+        /// Command to display ViewGrid located in lower right corner
+        /// </summary>
         protected AppBarButton ViewCommand;
+        /// <summary>
+        /// Command to display AddGrid located in lower right corner
+        /// </summary>
         protected AppBarButton AddCommand;
 
+        /// <summary>
+        /// Called when Add Button is pressed, and all entries are valid.
+        /// </summary>
         protected abstract void AddNewItem();
+        /// <summary>
+        /// Generates the header to display in ViewGrid.
+        /// </summary>
+        /// <returns>Generted header</returns>
         protected abstract Grid Header();
+        /// <summary>
+        /// Map controls of AddGrid presented in xaml file to the controls defined in this class.
+        /// </summary>
+        /// <param name="AddViewGridControls">Controls that will be mapped.</param>
         protected abstract void InitializeAddGrid(params FrameworkElement[] AddViewGridControls);
+        /// <summary>
+        /// Will be called automatically when an item is added or modified.
+        /// </summary>
+        /// <param name="Controls">Required to add all controls to this which are checked for validity.</param>
+        /// <param name="ErrorWaale">Controls whose inputs are invalid.</param>
         protected abstract void CheckInputs(LinkedList<Control> Controls, LinkedList<Control> ErrorWaale);
+        /// <summary>
+        /// Will be called automatically to clead AddGrid.
+        /// </summary>
         protected abstract void ClearAddGrid();
+        /// <summary>
+        /// Will be called when we have to modify item and all entries are valid
+        /// </summary>
         protected abstract void ItemToChangeUpdate();
+        /// <summary>
+        /// Will be called when a ListViewItem is selected (Not called when clicked on PointerOverObject).
+        /// </summary>
         protected abstract void SetContentDialog();
+        /// <summary>
+        /// Called to fill AddGrid controls when modification in an entry is required.
+        /// </summary>
         protected abstract void SetAddGrid_ItemToChange();
+        /// <summary>
+        /// Used to order the ViewGrid and lists
+        /// </summary>
+        /// <returns>Enumerable which contains the updated ordering of entries in lists</returns>
         protected abstract IOrderedEnumerable<T> OrderList();
+        /// <summary>
+        /// Clear all the controls and remove all ListViewItems in ViewGrid other than lists.
+        /// </summary>
         public abstract void DestructViews();
 
+        /// <summary>
+        /// Optional - Called when some tasks are required to be done when an item is deleted. (ItemToChange can be used for currently deleted item).
+        /// </summary>
         public virtual void PostDeleteTasks()
         {
-
+            // Kept empty and virtual because some childs may not use it
         }
 
+        /// <summary>
+        /// Must be called first to map each and every control from xaml file to this instance.
+        /// </summary>
+        /// <param name="viewGrid"></param>
+        /// <param name="addGrid"></param>
+        /// <param name="viewCommand"></param>
+        /// <param name="addCommand"></param>
+        /// <param name="AddViewGridControls"></param>
         public void InitializeViews(Grid viewGrid, Grid addGrid, AppBarButton viewCommand, AppBarButton addCommand, params FrameworkElement[] AddViewGridControls)
         {
             ViewGrid = viewGrid;
@@ -60,12 +132,16 @@ namespace ConsoleAppEngine.Abstracts
             AddGrid.Visibility = Visibility.Collapsed;
         }
 
+        /// <summary>
+        /// Set all Click Events of current instance
+        /// </summary>
         private void SetEvents()
         {
             ViewCommand.Click += (sender, e) =>
             {
                 AddGrid.Visibility = Visibility.Collapsed;
                 ViewGrid.Visibility = Visibility.Visible;
+                // Clear AddGrid when last item was set to modify but not modified
                 if (AddButton.Content.ToString() == "Modify")
                 {
                     ClearAddGrid();
@@ -81,12 +157,15 @@ namespace ConsoleAppEngine.Abstracts
             {
                 try
                 {
+                    // This will throw an exception when any control have invalid data
                     AbstractCheckInputs();
                 }
                 catch
                 {
                     return;
                 }
+
+                // This code will be called only when all inputs are Valid
                 if (AddButton.Content.ToString() == "Add")
                 {
                     AddNewItem();
@@ -96,19 +175,25 @@ namespace ConsoleAppEngine.Abstracts
                     ItemToChangeUpdate();
                     UpdateList();
                 }
+
+                // Change View to ViewGrid
                 ViewGrid.Visibility = Visibility.Visible;
                 AddGrid.Visibility = Visibility.Collapsed;
+
+                // Clear all the AddGrid Values
                 ClearAddGrid();
                 ItemToChange = null;
                 return;
             };
             ViewList.SelectionChanged += async (sender, e) =>
             {
+                // No item is selected or programmatically selected item was unselected
                 if (ViewList.SelectedItem == null)
                 {
                     return;
                 }
 
+                // Maps Selected item to ItemToChange
                 foreach (var a in lists)
                 {
                     if (a.GetView == ViewList.SelectedItem)
@@ -118,23 +203,29 @@ namespace ConsoleAppEngine.Abstracts
                     }
                 }
 
+                // Unselect the selected item
                 ViewList.SelectedItem = null;
 
+                // PointerOverObject checking 
                 if (ItemToChange.PointerOverObject != null &&
                 ItemToChange.PointerOverObject is ButtonBase x && x.IsPointerOver)
                 {
                     return;
                 }
 
+                // Update ContentDialog
                 SetContentDialog();
 
                 switch (await contentDialog.ShowAsync())
                 {
                     // DELETE
                     case ContentDialogResult.Secondary:
+                        // Remove Selected item
                         ViewList.Items.Remove(ItemToChange.GetView);
                         lists.Remove(ItemToChange);
                         ItemToChange.IsDeleted = true;
+
+                        // Call for operations after deleting
                         PostDeleteTasks();
                         ItemToChange = null;
                         break;
@@ -146,12 +237,16 @@ namespace ConsoleAppEngine.Abstracts
                         AddGrid.Visibility = Visibility.Visible;
 
                         AddButton.Content = "Modify";
+                        // Fill ViewGrid
                         SetAddGrid_ItemToChange();
                         break;
                 }
             };
         }
 
+        /// <summary>
+        /// Called when AddButton is clicked to check item for validitty.
+        /// </summary>
         private void AbstractCheckInputs()
         {
             LinkedList<Control> controls_cando = new LinkedList<Control>();
@@ -160,6 +255,7 @@ namespace ConsoleAppEngine.Abstracts
 
             CheckInputs(controls_cando, controls_err);
 
+            // Set every Control border to default
             foreach (Control x in controls_cando)
             {
                 x.BorderBrush = new SolidColorBrush(Color.FromArgb(102, 255, 255, 255));
@@ -167,6 +263,7 @@ namespace ConsoleAppEngine.Abstracts
 
             AddButton.BorderBrush = new SolidColorBrush(Color.FromArgb(102, 255, 255, 255));
 
+            // Set invalid controls border to red
             foreach (Control x in controls_err)
             {
                 x.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
@@ -179,6 +276,9 @@ namespace ConsoleAppEngine.Abstracts
             }
         }
 
+        /// <summary>
+        /// Called automatically and once when ViewGrid is initialised.
+        /// </summary>
         protected void FillViewGrid()
         {
             Grid header = Header();
@@ -192,33 +292,48 @@ namespace ConsoleAppEngine.Abstracts
             UpdateList();
         }
 
+        /// <summary>
+        /// Update lists and ViewList when an item is added or modified
+        /// </summary>
         protected void UpdateList()
         {
+            // If current instaance of ViewList is null
             if (ViewList == null)
             {
                 return;
             }
 
+            // Remove deleted items from list
             foreach (var a in (from x in lists where x.IsDeleted == true select x).ToArray())
             {
                 lists.Remove(a);
             }
 
+            // order all elements
             List<T> v = OrderList().ToList();
             lists.Clear();
+
+            // Update list by oedered elements
             foreach (var x in v)
             {
                 lists.AddLast(x);
             }
 
+            // Clear ViewList
             ViewList.Items.Clear();
 
+            // Fill ViewList with new ordering
             foreach (var a in from a in lists select a.GetView)
             {
                 ViewList.Items.Add(a);
             }
         }
 
+        /// <summary>
+        /// Automatically generates the header based on Title and Width of column in header
+        /// </summary>
+        /// <param name="Input">Array of Name of column and corrsponding StarWidth</param>
+        /// <returns></returns>
         protected static Grid GenerateHeader(params (string Name, double Width)[] Input)
         {
             Grid grid = new Grid() { Margin = new Thickness(10, 10, 10, 10) };
--- ConsoleAppEngine/Supportive Structures/Abstracts/ECourseElemItemBase.cs
+++ ConsoleAppEngine/Supportive Structures/Abstracts/ECourseElemItemBase.cs
@@ -6,18 +6,39 @@ namespace ConsoleAppEngine.Abstracts
 {
     public abstract class EElementItemBase
     {
+        /// <summary>
+        /// When an item is removed, it is set true and GC will collect later
+        /// </summary>
         public bool IsDeleted = false;
+        /// <summary>
+        /// ListViewItem to be displayed in View
+        /// </summary>
         public readonly ListViewItem GetView = new ListViewItem() { HorizontalContentAlignment = HorizontalAlignment.Stretch };
+        /// <summary>
+        /// An item in GetView, when clicked on it, will not display contentDialog
+        /// </summary>
         internal abstract object PointerOverObject { get; }
 
+        /// <summary>
+        /// Fill the given ListViewItem with current Entry and returns array of controls added
+        /// </summary>
+        /// <param name="GetView">ViewItem in which the current object view will be filled.</param>
+        /// <param name="Input">Decides the type of view and the width in * of Grid.Column.</param>
+        /// <returns></returns>
         protected static FrameworkElement[] GenerateViews(ListViewItem GetView, params (Type t, double Width)[] Input)
         {
+            // This grid will be added in GetView beause 
+            // 1. We have multiple controls to be added in single ListViewItem
+            // 2. Controls will be separated on basis of width of columns
             Grid grid = new Grid();
             FrameworkElement[] controls = new FrameworkElement[Input.Length];
 
             for (int i = 0; i < Input.Length; ++i)
             {
+                // Add Column for current control
                 grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(Input[i].Width, GridUnitType.Star) });
+
+                // Current Control is TextBox
                 if (Input[i].t.Equals(typeof(string)))
                 {
                     controls[i] = new TextBlock()
@@ -27,6 +48,7 @@ namespace ConsoleAppEngine.Abstracts
                         TextWrapping = TextWrapping.Wrap
                     };
                 }
+                // Current control is CheckBox
                 else if (Input[i].t.Equals(typeof(bool)))
                 {
                     controls[i] = new CheckBox()
@@ -36,6 +58,7 @@ namespace ConsoleAppEngine.Abstracts
                         MinWidth = 32
                     };
                 }
+                // Current control is Hyperlink (Not used anywhere, but can be used later)
                 else if (Input[i].t.Equals(typeof(HyperlinkButton)))
                 {
                     controls[i] = new HyperlinkButton()
@@ -44,10 +67,15 @@ namespace ConsoleAppEngine.Abstracts
                     };
                 }
 
+                // Add Control to grid
                 Grid.SetColumn(controls[i], i);
                 grid.Children.Add(controls[i]);
             }
+
+            // Add Grid to ViewItem
             GetView.Content = grid;
+
+            // Return the controls generated
             return controls;
         }
     }
