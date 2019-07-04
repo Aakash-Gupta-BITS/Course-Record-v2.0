05ff0653919e51232b4019f90b2258d05a3e8f12
Kalash Shah <kalashshah11@gmail.com>
2019-07-04 13:07:40 +0530

    Some Changes to Add Course

--- Course Record v2.0/Frames/Course/Add Course.xaml.cs
+++ Course Record v2.0/Frames/Course/Add Course.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -8,26 +9,34 @@ namespace Course_Record_v2._0.Frames.Course
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
     public sealed partial class Add_Course : Page
     {
         private readonly ConsoleAppEngine.Course.AllCourses Courses = new ConsoleAppEngine.Course.AllCourses ();
         public Add_Course()
         {
             this.InitializeComponent();
+            
         }
+        
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            byte i;
+            byte i=0;
+            byte length = (byte)System.Enum.GetNames(typeof(ConsoleAppEngine.AllEnums.CourseType)).Length;
             ConsoleAppEngine.AllEnums.CourseType type = new ConsoleAppEngine.AllEnums.CourseType();
-            for(type=0,i=0;i<27;i++)
-            {               
-                if(nameof(type) == TypeInput.ToString())
+            while(i<length)
+            {
+                string m = System.Enum.GetName(typeof(ConsoleAppEngine.AllEnums.CourseType), i);
+               
+                if(TypeInput.SelectedItem.ToString()==m)
                 {
+                    type = (ConsoleAppEngine.AllEnums.CourseType)i;
                     ConsoleAppEngine.Course.CourseEntry courseadd = new ConsoleAppEngine.Course.CourseEntry((type, IdInput.ToString()), TitleInput.ToString(), byte.Parse(LectureInput.ToString()), byte.Parse(PracticalInput.ToString()), null);
                     Courses.CoursesList.AddLast(courseadd);
                 }
-                type++;
+                i++;
+                
             }
             
            
