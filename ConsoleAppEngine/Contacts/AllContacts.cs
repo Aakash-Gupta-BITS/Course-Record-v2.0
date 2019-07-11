7e4af35aec898f69bfbdad53a014cf9e029a6d9a
Kalash Shah <kalashshah11@gmail.com>
2019-07-11 11:57:09 +0530

    Teachers and Students Add to Hdd done

--- ConsoleAppEngine/Contacts/AllContacts.cs
+++ ConsoleAppEngine/Contacts/AllContacts.cs
@@ -1,4 +1,12 @@
 ﻿using ConsoleAppEngine.Course;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using Windows.Storage;
+
 
 namespace ConsoleAppEngine.Contacts
 {
@@ -6,5 +14,40 @@ namespace ConsoleAppEngine.Contacts
     {
         public readonly ETeachers TeacherEntry = new ETeachers();
         public readonly EStudents StudentEntry = new EStudents();
+
+
+
+
+        public void AddToHdd()
+        {
+            string DirectoryLocation = Path.Combine(ApplicationData.Current.LocalFolder.Path, "Database", "Contacts");
+
+            if (!Directory.Exists(DirectoryLocation))
+            {
+                Directory.CreateDirectory(DirectoryLocation);
+            }
+
+            foreach (ETeacherEntry e in TeacherEntry.lists)
+            {
+                using (Stream m = new FileStream(Path.Combine(DirectoryLocation, "Teachers" + ".bin"), FileMode.Create, FileAccess.Write))
+                {
+                    new BinaryFormatter().Serialize(m, e);
+                }
+            }
+            DirectoryLocation = Path.Combine(ApplicationData.Current.LocalFolder.Path, "Database", "Contacts");
+
+            if (!Directory.Exists(DirectoryLocation))
+            {
+                Directory.CreateDirectory(DirectoryLocation);
+            }
+
+            foreach (EStudentEntry e in StudentEntry.lists)
+            {
+                using (Stream m = new FileStream(Path.Combine(DirectoryLocation, "Students" + ".bin"), FileMode.Create, FileAccess.Write))
+                {
+                    new BinaryFormatter().Serialize(m, e);
+                }
+            }
+        }
     }
 }
