a7b6493d48bd53ab662e395ec4c4249e622751f5
Kalash Shah <kalashshah11@gmail.com>
2019-07-13 14:49:18 +0530

    Adding WebPage Hyperlinks into the Main Page

--- Course Record v2.0/MainPage.xaml.cs
+++ Course Record v2.0/MainPage.xaml.cs
@@ -1,11 +1,11 @@
-﻿using ConsoleAppEngine.AllEnums;
-using ConsoleAppEngine.Contacts;
+﻿using ConsoleAppEngine.Contacts;
 using ConsoleAppEngine.Course;
 using ConsoleAppEngine.Log;
 using System.Collections.Generic;
 using Windows.Storage;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using System;
 
 namespace Course_Record_v2._0
 {
@@ -48,7 +48,7 @@ Vidya Vihar, Pilani 333031 (Rajasthan)",
             #endregion 
         }
 
-        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        private async void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
             if (sender.SelectedItem == null)
             {
@@ -73,6 +73,122 @@ Vidya Vihar, Pilani 333031 (Rajasthan)",
                 lis.AddLast(Courses);
                 this.Frame.Navigate(typeof(Frames.Contacts.MainPage), lis);
             }
+            else if (SelectedItem == SWD)
+            {
+                var uriBing = new Uri(@"http://swd.bits-pilani.ac.in");
+                var options = new Windows.System.LauncherOptions();
+                // Launch the URI with a warning prompt
+                options.TreatAsUntrusted = false;
+                var success = await Windows.System.Launcher.LaunchUriAsync(uriBing);
+
+                if (success)
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage opened successfully");
+                    NavView.SelectedItem = null;
+                }
+                else
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage was not opened");
+                }
+
+            }
+            else if (SelectedItem == ERP)
+            {
+                var uriBing = new Uri(@"http://erp.bits-pilani.ac.in");
+                var options = new Windows.System.LauncherOptions();
+                // Launch the URI with a warning prompt
+                options.TreatAsUntrusted = false;
+                var success = await Windows.System.Launcher.LaunchUriAsync(uriBing);
+
+                if (success)
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage " + SelectedItem.Content.ToString() + " opened successfully");
+                    NavView.SelectedItem = null;
+                }
+                else
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage " + SelectedItem.Content.ToString() + " was not opened");
+                }
+
+            }
+            else if (SelectedItem == Nalanda)
+            {
+                var uriBing = new Uri(@"http://nalanda.bits-pilani.ac.in");
+                var options = new Windows.System.LauncherOptions();
+                // Launch the URI with a warning prompt
+                options.TreatAsUntrusted = false;
+                var success = await Windows.System.Launcher.LaunchUriAsync(uriBing);
+
+                if (success)
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage " + SelectedItem.Content.ToString() + " opened successfully");
+                    NavView.SelectedItem = null;
+                }
+                else
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage " + SelectedItem.Content.ToString() + " was not opened");
+                }
+
+            }
+            else if (SelectedItem == ID)
+            {
+                var uriBing = new Uri(@"http://id");
+                var options = new Windows.System.LauncherOptions();
+                // Launch the URI with a warning prompt
+                options.TreatAsUntrusted = false;
+                var success = await Windows.System.Launcher.LaunchUriAsync(uriBing);
+
+                if (success)
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage " + SelectedItem.Content.ToString() + " opened successfully");
+                    NavView.SelectedItem = null;
+                }
+                else
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage " + SelectedItem.Content.ToString() + " was not opened");
+                }
+
+            }
+            else if (SelectedItem == RC)
+            {
+                var uriBing = new Uri(@"http://rc.bits-pilani.ac.in/");
+                var options = new Windows.System.LauncherOptions();
+                // Launch the URI with a warning prompt
+                options.TreatAsUntrusted = false;
+                var success = await Windows.System.Launcher.LaunchUriAsync(uriBing);
+
+                if (success)
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage " + SelectedItem.Content.ToString() + " opened successfully");
+                    NavView.SelectedItem = null;
+                }
+                else
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage " + SelectedItem.Content.ToString() + " was not opened");
+                }
+
+            }
+            else if (SelectedItem == LIB)
+            {
+                var uriBing = new Uri(@"http://www.bits-pilani.ac.in:12354/");
+                var options = new Windows.System.LauncherOptions();
+                // Launch the URI with a warning prompt
+                options.TreatAsUntrusted = false;
+                var success = await Windows.System.Launcher.LaunchUriAsync(uriBing);
+
+                if (success)
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage " + SelectedItem.Content.ToString() + " opened successfully");
+                    NavView.SelectedItem = null;
+                }
+                else
+                {
+                    LoggingServices.Instance.WriteLine<MainPage>("The WebPage " + SelectedItem.Content.ToString() + " was not opened");
+                }
+
+            }
+
+
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -84,5 +200,6 @@ Vidya Vihar, Pilani 333031 (Rajasthan)",
             Courses.AddToHdd_NewThread();
             Contacts.AddToHdd_NewThread();
         }
+        
     }
 }
