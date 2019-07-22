db3aaf195d8f1d66cd5ac930d6e847a14f4549fa
Kalash Shah <kalashshah11@gmail.com>
2019-07-22 15:53:54 +0530

    Addition of Uri is done correctly in Combo Box

--- Course Record v2.0/Frames/FeedBack.xaml.cs
+++ Course Record v2.0/Frames/FeedBack.xaml.cs
@@ -9,6 +9,11 @@ namespace Course_Record_v2._0.Frames
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    public struct uriEdit
+    {
+        string Name;
+        Uri uriE;
+    }
     public sealed partial class FeedBack : Page
     {
         public FeedBack()
@@ -21,16 +26,29 @@ namespace Course_Record_v2._0.Frames
             webView1.NavigationCompleted += (sender, args) =>
             {
                 Progress.IsActive = false;
+                Update();
             };
         }
-        public void Update()
+        public async void Update()
         {
             Uri uriForm = new Uri("https://docs.google.com/forms/d/e/1FAIpQLSexBkTA-zBSAeQPd3M24wXCIJPXc31YAJ61U2uusFSegF-VzA/formResponse");
               
-            if(webView1.Source == uriForm)
+            if(webView1.Source.ToString() == uriForm.ToString())
             {
-                
-                LoggingServices.Instance.WriteLine<FeedBack>("Hello" );
+                char ch;
+                string html = await webView1.InvokeScriptAsync("eval", new string[] { "document.documentElement.outerHTML;" });
+                int index1 = html.IndexOf("freebirdFormviewerViewResponseLinksContainer");
+                int index2 = html.IndexOf("https", index1);
+                string uriE="";
+                int i = index2;
+                for(i=index2; html[i+1] != 34; ++i)
+                {         
+                    ch = html[i];
+                    uriE = uriE + html[i];
+                    if (ch == 63)
+                    { i = i + 21; }
+                }
+                combo.Items.Add(uriE);
             }
         }
     }
