64f86e430fdf94c5f0a935f08e8c7c949450e2b0
Aakash Gupta <f20180887@pilani.bits-pilani.ac.in>
2019-08-22 13:36:00 +0530

    PDF Parser Project Added

---
+++ Admin Operations/Data Parser.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin_Operations
+{
+    public enum CompreSession
+    {
+        AM,
+        PM
+    }
+
+    public class TimeEntry
+    {
+        public int Section { get; set; }
+        public string Teacher { get; set; }
+        public string Room { get; set; }
+        public LinkedList<DayOfWeek> Days { get; set; }
+        public LinkedList<int> Hours { get; set; }
+    }
+
+    public class Course
+    {
+        public string COM_COD { get; set; }
+        public string Number { get; set; }
+        public string Title { get; set; }
+        public (int L, int P, int U) Credits { get; set; }
+        public (DateTime date, CompreSession session) CompreTiming { get; set; }
+
+        public Course(LinkedList<string[]> Entry)
+        {
+            COM_COD = Entry.First.Value[0];
+            Number = Entry.First.Value[1];
+            Title = Entry.First.Value[2];
+            Credits = (int.Parse(Entry.First.Value[3]), int.Parse(Entry.First.Value[4]), int.Parse(Entry.First.Value[5]));
+        }
+    }
+
+    public class MainParser
+    {
+        readonly LinkedList<LinkedList<string[]>> Inputs;
+
+        public LinkedList<Course> Courses { get; private set; }
+
+        public MainParser(string[][] input)
+        {
+            for (int i = 0; i < input.GetLength(0); ++i)
+            {
+                if (input[i][0] == "")
+                    throw new Exception();
+
+                int upto = i;
+                // Test remaining for this loop
+                for (int j = i; j < input.GetLength(0) - 1; ++j)
+                    if (input[j + 1][0] == "")
+                        ++upto;
+                    else
+                        break;
+
+                LinkedList<string[]> temp = new LinkedList<string[]>();
+                for (int j = i; j <= upto; ++j)
+                {
+                    temp.AddLast(input[j]);
+                }
+
+                Inputs.AddLast(temp);
+
+                i = upto;
+            }
+
+        }
+    }
+}
---
+++ Admin Operations/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.Serialization.Formatters.Binary;
+using SautinSoft;
+
+namespace Admin_Operations
+{
+
+    class PDFParse
+    {
+        readonly PdfFocus pdfFocus;
+        readonly Application xlApp;
+
+        public PDFParse(string location)
+        {
+            Console.WriteLine("Opening PDF from " + location);
+            pdfFocus = new PdfFocus();
+            pdfFocus.OpenPdf(location);
+
+            Console.WriteLine("Creating Excel Object");
+            xlApp = new Application();
+
+            if (xlApp == null)
+                throw new Exception("Excel not installed!!");
+        }
+
+        public string[,] GetPage(int PageNumber, string exportDir, string Format)
+        {
+            string fileloc = string.Format(Path.Combine(exportDir, string.Format(Format, PageNumber)));
+            Console.WriteLine("Parsing Page {0} to excel...", PageNumber);
+            pdfFocus.ToExcel(fileloc, PageNumber, PageNumber);
+            Console.WriteLine("Parsing Page {0} Completed...", PageNumber);
+
+            Console.WriteLine("Loading Page {0} to memory", PageNumber);
+
+            Workbook xlWorkBook = xlApp.Workbooks.Open(fileloc);
+
+            string[,] Output = null;
+
+            int count = 0;
+
+            foreach (Worksheet xlWorkSheet in xlWorkBook.Sheets)
+            {
+                if (count != 0)
+                    break;
+                ++count;
+                Console.WriteLine("Opening Worksheet");
+                Range xlRange = xlWorkSheet.UsedRange;
+                int totalRows = xlRange.Rows.Count;
+                int totalColumns = xlRange.Columns.Count;
+
+                Output = new string[totalRows, totalColumns];
+
+                for (int i = 7; i <= totalRows; i++)
+                    for (int j = 1; j <= totalColumns; ++j)
+                        Output[i - 7, j - 1] = (xlRange.Cells[i, j] as Range).Text.ToString().Replace("\n", "");
+
+                Marshal.ReleaseComObject(xlWorkSheet);
+            }
+
+            xlWorkBook.Close();
+            Marshal.ReleaseComObject(xlWorkBook);
+
+            return Output;
+        }
+
+        public void End()
+        {
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            string DirLoc = "";
+            string FileLoc;
+            do
+            {
+                Console.Write("Enter File Location : ");
+                FileLoc = Console.ReadLine();
+                if (FileLoc.Contains("\\"))
+                    DirLoc = FileLoc.Substring(0, FileLoc.LastIndexOf("\\"));
+
+            } while (!File.Exists(FileLoc) || !FileLoc.EndsWith(".pdf"));
+
+            string ExcelExportLocation = Path.Combine(DirLoc, "Excel Files");
+            string BinExportLocation = Path.Combine(DirLoc, "Bin Files");
+
+
+            if (!Directory.Exists(ExcelExportLocation))
+                Directory.CreateDirectory(ExcelExportLocation);
+            if (!Directory.Exists(BinExportLocation))
+                Directory.CreateDirectory(BinExportLocation);
+
+            PDFParse p = new PDFParse(FileLoc);
+
+            for (int i = 1; i <= 48; ++i)
+            {
+                using (Stream m = new FileStream(Path.Combine(BinExportLocation, i + ".bin"), FileMode.Create, FileAccess.Write))
+                {
+                    new BinaryFormatter().Serialize(m, p.GetPage(i, ExcelExportLocation, "{0}.xls"));
+                }
+            }
+
+            p.End();
+        }
+    }
+}
